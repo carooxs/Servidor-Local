7bf6988807152bda0e7581d0bd0e7c1a4a755a18
carooxs <c.nogueira.souza@gmail.com>
2019-05-28 22:16:04 -0300

    Cria nosso primeiro Servidor com Nodemon e duas Rotas

--- server.js
+++ server.js
@@ -1,1 +1,19 @@
 const http = require('http')
+
+const servidor = http.createServer(function (request, response){
+    if (request.url ==='/') {
+        response.end('Hello Wonderful World!')
+    } else if (request.url === '/comidas') {
+        
+        if (request.method === 'GET'){
+          response.writeHead(200, {"Content-Type": "text/html;charset=utf-8"})  
+          response.end("<h1>Respostão Grandão Diferentão</h1>")  
+        }else  if (request.method === 'POST'){
+            response.writeHead(200, {"Content-Type": "text/html;charset=utf-8"})  
+            response.end("<h1>Respostão Grandão Diferentão da moléstia do post</h1>")  
+          }
+          
+    }
+}) 
+servidor.listen(3000)
+console.log('Servidorzinho rodando na porta 3000');
