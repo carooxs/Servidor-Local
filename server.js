95d27623a96453648aa4f246f4bb8bb094f009a0
carooxs <c.nogueira.souza@gmail.com>
2019-05-29 22:17:09 -0300

    Criação de objeto no Server

--- server.js
+++ server.js
@@ -1,4 +1,29 @@
 const http = require('http')
+const comidas = {
+    pratosFavoritos: [
+  {
+    nome: "Batata frita",
+    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+    imagem: "img/Batata-frita.jpg"
+  },
+  {
+    nome: "Macarronada",
+    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+    imagem: "img/macarronada.jpg"
+  },
+  {
+    nome: "Falafel",
+    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+    imagem: "img/falafel.jpg"
+  },
+  {
+    nome: "Creme de abóbora",
+    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
+    imagem: "img/creme-de-abobora.jpg"
+  }
+
+]
+}
 
 const servidor = http.createServer(function (request, response){
     if (request.url ==='/') {
@@ -6,10 +31,13 @@ const servidor = http.createServer(function (request, response){
     } else if (request.url === '/comidas') {
         
         if (request.method === 'GET'){
-          response.writeHead(200, {"Content-Type": "text/html;charset=utf-8"})  
-          response.end("<h1>Respostão Grandão Diferentão</h1>")  
+          response.writeHead(200, {"Content-Type": "aplication/json", "Access-Control-Allow-Origin": "*"})  
+
+          response.write(JSON.stringify(comidas))
+           
+          response.end()  
         }else  if (request.method === 'POST'){
-            response.writeHead(200, {"Content-Type": "text/html;charset=utf-8"})  
+            response.writeHead(201, {"Content-Type": "text/html;charset=utf-8"})  
             response.end("<h1>Respostão Grandão Diferentão da moléstia do post</h1>")  
           }
           
