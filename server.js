7dd35b28cddbd60cada72035fb3130d228a1fe7a
carooxs <c.nogueira.souza@gmail.com>
2019-05-30 21:52:29 -0300

    Utilizando o express, código limpo, cors no server

--- server.js
+++ server.js
@@ -1,33 +1,15 @@
 const express = require('express')
-const comidas = {
-    pratosFavoritos: [
-  {
-    nome: "Batata frita",
-    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-    imagem: "img/Batata-frita.jpg"
-  },
-  {
-    nome: "Macarronada",
-    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-    imagem: "img/macarronada.jpg"
-  },
-  {
-    nome: "Falafel",
-    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-    imagem: "img/falafel.jpg"
-  },
-  {
-    nome: "Creme de abóbora",
-    descricao: "Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus. ",
-    imagem: "img/creme-de-abobora.jpg"
-  }
+const cors = require ('cors')
+const controller = require('./ComidasController')
 
-]
-}
+
+
 
 const servidor = express()
+servidor.use(cors())
+
 servidor.get("/comidas", (request, response) => {
-  response.send("Boa noite, amigas!")
+  response.send(controller.getAll())
 
 })
 
